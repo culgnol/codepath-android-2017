7bedfc1034d7be91f5e90d9a4d3e34bc536e381d
Long Luc <long.luc@gatech.edu>
2017-09-10 18:21:53 -0700

    wk2 add custom layout file
    update ArticleArrayAdapter to use custom view to load thumbnails in getView

--- week-2/nytarticlesearch/app/src/main/java/com/codepath/nytarticlesearch/ArticleArrayAdapter.java
+++ week-2/nytarticlesearch/app/src/main/java/com/codepath/nytarticlesearch/ArticleArrayAdapter.java
@@ -1,8 +1,19 @@
 package com.codepath.nytarticlesearch;
 
 import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -14,5 +25,36 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
 
     public ArticleArrayAdapter(Context context, List<Article> articles) {
         super(context, android.R.layout.simple_list_item_1, articles);
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // get the data item for position
+        Article article = this.getItem(position);
+
+        // check to see if existing view being reused
+        // not using a recycled view -> inflate the layout
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_article_result, parent, false);
+
+        }
+        // find the image view
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivImage);
+
+        // clear out recycle image from convertView from last time
+        imageView.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        tvTitle.setText(article.getHeadline());
+
+        // populate the thumbnail image
+        // remote downlooad the image in the background
+        String thumbnail = article.getThumbnail();
+        if(!TextUtils.isEmpty(thumbnail)) {
+            Picasso.with(getContext()).load(thumbnail).into(imageView);
+        }
+
+        return convertView;
     }
 }
