d64e28383502ef8c5ab31e045c61636c2fe744c7
Long Luc <long.luc@gatech.edu>
2017-09-10 18:03:35 -0700

    add week 2 assignment
    Setup AsyncHttpclient
    Setup Model with factory method

---
+++ week-2/nytarticlesearch/app/src/main/java/com/codepath/nytarticlesearch/Article.java
@@ -0,0 +1,67 @@
+package com.codepath.nytarticlesearch;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * Created by culgnol on 9/10/17.
+ */
+
+/// Model
+
+public class Article {
+
+    String weburl;
+    String headline;
+    String thumbnail;
+
+    public String getWeburl() {
+        return weburl;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public Article(JSONObject jsonObject) {
+        try {
+            this.weburl = jsonObject.getString("web_url");
+            this.headline = jsonObject.getJSONObject("headline").getString("main");
+
+            JSONArray multimedia = jsonObject.getJSONArray("multimedia");
+
+            if (multimedia.length() > 0) {
+                JSONObject multimediaJson = multimedia.getJSONObject(0); // get first found image
+                this.thumbnail = "http://www.nytimes.com/" + multimediaJson.getString("url");
+            } else {
+                this.thumbnail = "";
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // factory method to parse to list of Article objects
+    public static ArrayList<Article> fromJsonArray(JSONArray array) {
+        ArrayList<Article> results = new ArrayList<>();
+
+        for(int x = 0; x < array.length(); x++) {
+            try {
+                results.add(new Article(array.getJSONObject(x)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+}
